4e5d285e3512597b7f794c4bbdce11339a5e9d6b
andres velasco <velasco.andrs@gmail.com>
2020-10-28 23:29:45 -0500

    persistencia de datos del bloc

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -4,7 +4,16 @@ import 'package:flutter/material.dart';
 class Provider extends InheritedWidget {
   final loginBloc = LoginBloc();
 
-  Provider({Key key, Widget child}) : super(key: key, child: child);
+  static Provider _instancia;
+
+  factory Provider({Key key, Widget child}) {
+    if (_instancia == null) {
+      _instancia = new Provider._internal(key: key, child: child);
+    }
+    return _instancia;
+  }
+
+  Provider._internal({Key key, Widget child}) : super(child: child, key: key);
 
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
